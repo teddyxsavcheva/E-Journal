58b20db886aae06539a10d74f3aff868570ce79b
Teodora Savcheva <teddyxsavcheva02@gmail.com>
2024-06-25 03:13:56 +0300

    Tutorial is done, now i just need to work with the roles and laso test the whole backend

--- E-Journal-Group-Project/src/main/java/com/nbu/ejournalgroupproject/auth/AuthenticationService.java
+++ E-Journal-Group-Project/src/main/java/com/nbu/ejournalgroupproject/auth/AuthenticationService.java
@@ -1,9 +1,12 @@
 package com.nbu.ejournalgroupproject.auth;
 
+import com.nbu.ejournalgroupproject.config.JwtService;
 import com.nbu.ejournalgroupproject.user.Role;
 import com.nbu.ejournalgroupproject.user.User;
 import com.nbu.ejournalgroupproject.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -11,8 +14,10 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class AuthenticationService {
 
-    private final UserRepository userRepository;
+    private final UserRepository repository;
     private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager manager;
 
     public AuthenticationResponse registerAdmin(RegisterRequest request) {
 
@@ -24,14 +29,35 @@ public class AuthenticationService {
                 .role(Role.ADMINISTRATOR)
                 .build();
 
+        repository.save(user);
+
+        String jwtToken = jwtService.generateToken(user);
+
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+
     }
 
 
     public AuthenticationResponse authenticateAdmin(AuthenticationRequest request) {
 
+        manager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+
+        User user = repository.findByEmail(request.getEmail())
+                .orElse(null);
+
+        String jwtToken = jwtService.generateToken(user);
+
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
 
 
     }
-
-
 }
